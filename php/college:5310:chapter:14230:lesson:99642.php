39ef9099bc15e23334bc6e1a400a9b601084fd5f
Mehrnoosh_Khorrami <khooorrami.m@gmail.com>
2025-04-16 20:40:50 +0330

    Answer to Quera php exercise

---
+++ php/college:5310:chapter:14230:lesson:99642.php
@@ -0,0 +1,17 @@
+<?php
+
+trait Singleton
+{
+    private static $instance = null;
+    private function __construct()
+    {
+
+    }
+    public static function getInstance()
+    {
+        if(self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
